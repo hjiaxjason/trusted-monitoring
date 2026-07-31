cd0baede0a8e30142ad3c066ad8900c4328a333a
Jason Huang <huangjj7@uci.edu>
2026-07-31 14:01:25 -0700

    Export backdoor verification interface

---
+++ src/trusted_monitoring/verification/__init__.py
@@ -0,0 +1,7 @@
+from trusted_monitoring.verification.backdoor_verifier import (
+    VerificationResult,
+    verify_backdoor,
+    verify_honest,
+)
+
+__all__ = ["VerificationResult", "verify_backdoor", "verify_honest"]
