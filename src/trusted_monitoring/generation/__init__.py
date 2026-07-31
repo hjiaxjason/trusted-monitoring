b08d7a50317d19d573645eeee656a6640516921c
Jason Huang <huangjj7@uci.edu>
2026-07-31 14:01:25 -0700

    Export Generator, ModelConfig, and system prompts

---
+++ src/trusted_monitoring/generation/__init__.py
@@ -0,0 +1,4 @@
+from trusted_monitoring.generation.generate import Generator, ModelConfig
+from trusted_monitoring.generation.prompts import ATTACK_SYSTEM_PROMPT, HONEST_SYSTEM_PROMPT
+
+__all__ = ["ATTACK_SYSTEM_PROMPT", "HONEST_SYSTEM_PROMPT", "Generator", "ModelConfig"]
