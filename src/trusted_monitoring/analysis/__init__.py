97985ec2cc6e385b815b9a820eff0de8fb15f867
Jason Huang <huangjj7@uci.edu>
2026-07-31 14:01:25 -0700

    Export pareto frontier and plotting functions

---
+++ src/trusted_monitoring/analysis/__init__.py
@@ -0,0 +1,4 @@
+from trusted_monitoring.analysis.pareto import compute_pareto_frontier
+from trusted_monitoring.analysis.plots import plot_monitor_strength_sweep, plot_pareto_frontier
+
+__all__ = ["compute_pareto_frontier", "plot_monitor_strength_sweep", "plot_pareto_frontier"]
